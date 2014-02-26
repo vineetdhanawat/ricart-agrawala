972d50edef1ab4d450598b3329b1030ca2c9917d
Vineet Dhanawat <vineetdhanawat@gmail.com>
2014-02-26 02:48:32 -0600

    Main File

---
+++ src/RicartAgrawala.java
@@ -0,0 +1,32 @@
+public class RicartAgrawala 
+{
+	// Total number of nodes in the system
+	public static int NUMNODES = 0;
+	
+	public static void main(String[] args)
+	{		
+		
+		// User will let the node know its nodeID
+		int nodeID = 0;
+		if (args.length > 0)
+		{
+			try
+			{
+		    	nodeID = Integer.parseInt(args[0]);
+		    }
+			catch (NumberFormatException e)
+			{
+				System.err.println("Argument must be an integer");
+				System.exit(1);
+		    }
+		}
+		try 
+		{
+			// Do Calculations
+		} 
+		catch (Exception e)
+		{
+			//TODO add error handling
+		}
+	}
+}
